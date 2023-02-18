e746333842fd95cd88783dfc1a6a977c73ed99d8
tanaka <mimomotoringo@yahoo.co.jp>
2023-02-19 01:31:47 +0900

    has_many, belongs_to を記述

--- app/models/article.rb
+++ app/models/article.rb
@@ -26,6 +26,7 @@ class Article < ApplicationRecord
 
   validate :validate_title_and_content_length
 
+  belongs_to :user
 
   def display_created_at
     I18n.l(created_at, format: :default)
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+
+  has_many :articles, dependent: :destroy
+        end
